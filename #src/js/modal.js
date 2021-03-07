bc9edc5b5f13fbc7b41a0a8260b5ce6a22ec5e90
disaq1 <slava79198015981@yandex.ru>
2021-03-07 18:52:42 +0400

    Прописывает логику open/close модального окна

---
+++ #src/js/modal.js
@@ -0,0 +1,17 @@
+﻿let popup = document.getElementById('popup');
+let popupToggle = document.getElementById('btnPopup');
+let popupClose = document.querySelector('.close');
+
+popupToggle.onclick = function() {
+    popup.classList.add('popup--opened');
+};
+
+popupClose.onclick = function() {
+    popup.classList.remove('popup--opened');
+};
+
+window.onclick = function(e) {
+    if(e.target == popup) {
+        popup.classList.remove('popup--opened');
+    }
+}
