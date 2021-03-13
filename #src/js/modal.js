5648cbecc109e21fe0f09a7cf6893c64e7167bcf
disaq1 <slava79198015981@yandex.ru>
2021-03-13 12:51:55 +0400

    fix скрипта модального окна

--- #src/js/modal.js
+++ #src/js/modal.js
@@ -4,14 +4,17 @@ let popupClose = document.querySelector('.close');
 
 popupToggle.onclick = function() {
     popup.classList.add('popup--opened');
+    document.body.classList.add('lock');
 };
 
 popupClose.onclick = function() {
     popup.classList.remove('popup--opened');
+    document.body.classList.remove('lock');
 };
 
 window.onclick = function(e) {
     if(e.target == popup) {
         popup.classList.remove('popup--opened');
+        document.body.classList.remove('lock');
     }
 }
