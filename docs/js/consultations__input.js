cf677b142a859cb8fffa45d8444aeb3cba31de1d
disaq1 <slava79198015981@yandex.ru>
2021-03-13 12:56:25 +0400

    прочее (скомпилированное gulp'ом)

---
+++ docs/js/consultations__input.js
@@ -0,0 +1,9 @@
+let diagnoz = document.getElementById("diagnoz");
+let alcko = document.getElementById("alcko");
+let zdorivie = document.getElementById("zdorivie");
+let mother = document.getElementById("mother");
+let other = document.getElementById("other");
+
+other.addEventListener('change', function(){
+    alert('qwe')
+})
